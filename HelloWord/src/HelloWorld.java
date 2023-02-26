32d06209dbcee79cd6adbd92dc0d458c6d17a768
shanyun <tang_mingxiang@163.com>
2023-02-26 22:28:11 +0800

    已学完第13讲

---
+++ HelloWord/src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 2;
+        char b = '2';
+        long c = 44444422222224L;
+        float f = 293.0f;
+        double db = 3242252;
+        a = (int) Math.pow(2, 31);
+        System.out.println(1 == 2 != false);
+        System.out.println(10 == 10.0);
+        System.out.println(a);
+        System.out.println(0.3 % 0.2);
+        System.out.println("Hello World！");
+        System.out.println(5 + 6);
+        System.out.println(5 / 6.0);
+        System.out.println(Math.pow(2, 4));
+    }
+}
